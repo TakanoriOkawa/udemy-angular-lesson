aa28a7e1b916a8bcd13813afd7fba99b9ea06f90
Taka <okawatakanori9916@gmail.com>
2022-12-24 23:29:06 +0900

    メンバーコンポーネント作成

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css']
+})
+export class MembersComponent {
+
+  member = "田中";
+
+  // ライフサイクルメソッドが入ってくる
+}
